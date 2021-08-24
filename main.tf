59cd7e541be32d6bfa263ae151300d348a0eb20a
nageshkkini <89379541+nageshkkini@users.noreply.github.com>
2021-08-24 09:56:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+  
+  required_version = ">=0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region = "us-east-2"
+}
+
 resource "aws_s3_bucket" "b" {
   bucket = "Nagesh_TF_ASSIGN"
   acl    = "private"
 
   tags = {
-    Name        = "Nagesh_TERRAFORM_BUCKET"
+    Name        = var.Bucket_name
     Environment = "Dev"
   }
 }
